9ba9415d02d20451d16b8fb270dcdea522bea4cc
Ababioaminatar <106369245+Ababioaminatar@users.noreply.github.com>
2022-07-01 11:35:26 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+simple calculator
+created by Aminatar Ayieni
+on July, 2022
+C89 plier
+MIT
+licence
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum,diff,product;
+    float quotient
+    print('"/simple calculator!/")
+    printf("enter two numbers:");
+    scanf(%d%d,num1,$um2);
+    //computation
+    sum=num1+num2
+    diff=num1-num2
+    product=num*num2
+    quotient=(float)num1/num2
+    //output
+    printf("%d+%d+%d/n",num1,num2,sum);
+       printf("%d-%d=%d/n"num1,num2,diff);
+       printf("%d*%d=%d/n",num1,num2,product);
+       printf("%d/%d=%f/n",num1,num2,quotient);
+
+
+    printf("/simple calculator/n")
+    return 0;
+}
